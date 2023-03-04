28803b0aae897af66c80aeb1ddf7efd073ae092a
yoshiyahoo <josiahtroup@gmail.com>
2023-03-04 12:44:26 -0800

    window pop_up not working

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use sfml::window::{ContextSettings, Window, Style};
+use sfml::graphics::{Color, RenderWindow, RenderTarget};
 fn main() {
-    println!("Hello, world!");
+    let mut window = RenderWindow::new(
+        (800, 600),
+        "SFML example",
+        Style::CLOSE,
+        &ContextSettings::default()
+    );
+
+    window.set_vertical_sync_enabled(true);
+
+    while let Some(ev) = window.poll_event() {
+        match ev {
+            sfml::window::Event::Closed | sfml::window::Event::KeyPressed { code: sfml::window::Key::Escape, .. } => window.close(),
+            _ => ()
+        }
+        window.clear(Color::BLACK);
+        window.display();
+    }
 }
